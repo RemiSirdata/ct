b35972b8fd8a51d4fb5c23d3f6f6c92c6815bc75
Rémi DEMOL (Freelance) <momolrd@gmail.com>
2015-04-17 04:13:00 +0200

    add changes

--- private/appointment.php
+++ private/appointment.php
@@ -1,16 +1,16 @@
 <?php
 
 function getVar($name){
-    return isset($_GET[$name]) ? $_GET[$name] : '';
+    return isset($_POST[$name]) ? $_POST[$name] : '';
 }
 
 
 $content = "Adresse IP : ".$_SERVER['SERVER_ADDR']."\n";
-$content = "Nom : ".getVar('name') . "\n";
-$content = "Téléphone : ".getVar('phone') . "\n";
-$content = "Email : ".getVar('email') . "\n";
-$content = "Date : ".getVar('date') . "\n";
-$content = "Message : ".getVar('message') . "\n";
+$content .= "Nom : ".getVar('name') . "\n";
+$content .= "Téléphone : ".getVar('phone') . "\n";
+$content .= "Email : ".getVar('email') . "\n";
+$content .= "Date : ".getVar('date') . "\n";
+$content .= "Message : ".getVar('message') . "\n";
 
 mail('osteo.ig@gmail.com','Nouveau rendez vous de '.getVar('name'),$content);
 
